c6448cd5784ed8f4253c1a71910b5254d2a813c7
Pradeep Gaud <pradeepbrandmingo.com>
2026-09-17 19:06:40 +0530

    isusdfduhbsd fixed

--- frontend/src/pages/PrivacyPage.jsx
+++ frontend/src/pages/PrivacyPage.jsx
@@ -6,6 +6,7 @@ export const PrivacyPage = () => {
   const navigate = useNavigate();
   
 
+
   return (
     <div className="min-h-screen bg-slate-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl mx-auto bg-white p-8 rounded-2xl shadow-sm border border-slate-200">

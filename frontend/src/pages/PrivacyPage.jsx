1b1bf9cb2c396a00deccaa79322fadf3fe0da427
Pradeep Gaud <pradeepbrandmingo.com>
2026-09-17 19:00:16 +0530

    done and policy page done and setup

--- frontend/src/pages/PrivacyPage.jsx
+++ frontend/src/pages/PrivacyPage.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from '../router';
 
 export const PrivacyPage = () => {
   const navigate = useNavigate();
+  
 
   return (
     <div className="min-h-screen bg-slate-50 py-12 px-4 sm:px-6 lg:px-8">
